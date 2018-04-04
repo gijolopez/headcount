5647898df361e4c263a9eb3074abd828fcc0e321
gijolopez <glopez22@me.com>
2018-04-04 14:33:03 -0600

    added two methods - parse timeframe & parse data

--- lib/parser.rb
+++ lib/parser.rb
@@ -3,4 +3,16 @@ module Parser
   def parse_name(row)
     row[:name] = row[:location].upcase
   end
+
+  def parse_timeframe(row)
+    row[:timeframe] = row[:timeframe].to_i
+  end
+  
+  def parse_data(row)
+    if row[:data] == 'N/A' || row[:data] == 'LNE' || row[:data] == "#VALUE!"
+      row[:data] = 'N/A'
+    else
+      row[:data] = ((row[:data].to_f)*1000).floor/1000.0
+    end
+  end
 end
