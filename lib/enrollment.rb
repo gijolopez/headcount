e57d6aef251482639f837e65ab94ed70308c27c1
gijolopez <glopez22@me.com>
2018-04-04 14:31:15 -0600

    added kindergarten participation instance variable to Enrollment file

--- lib/enrollment.rb
+++ lib/enrollment.rb
@@ -2,9 +2,11 @@ require 'csv'
 
 class Enrollment
   attr_reader :name
+  attr_accessor :kindergarten_participation
 
   def initialize(data)
     @name = data[:name]
+    @kindergarten_participation = data[:kindergarten_participation] || Hash.new
   end
 
 end
