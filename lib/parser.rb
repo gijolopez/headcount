9a2427898e1e667f6e9a4e5bb76b9f358c7d791a
gijolopez <glopez22@me.com>
2018-04-05 17:45:32 -0600

    added convert_deceimals_to_three_points method

--- lib/parser.rb
+++ lib/parser.rb
@@ -7,7 +7,11 @@ module Parser
   def parse_timeframe(row)
     row[:timeframe] = row[:timeframe].to_i
   end
-  
+
+  def convert_to_three_decimals(decimal)
+    (decimal.to_f*1000).floor/1000.0
+  end
+
   def parse_data(row)
     if row[:data] == 'N/A' || row[:data] == 'LNE' || row[:data] == "#VALUE!"
       row[:data] = 'N/A'
