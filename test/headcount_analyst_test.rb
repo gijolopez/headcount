aeed1f52683e1d1b66192204443fdff5c5653c8f
gijolopez <glopez22@me.com>
2018-04-06 14:23:24 -0600

    trying to get headcount_analyst_test to pass

--- test/headcount_analyst_test.rb
+++ test/headcount_analyst_test.rb
@@ -3,9 +3,25 @@ require_relative '../lib/headcount_analyst'
 
 class HeadcountAnalystTest < Minitest::Test
 
-  def test_it_exisit
-    ha = HeadcountAnalyst.new
+  def setup
+    @dr = DistrictRepository.new(load_data)
+    load_data = ({:enrollment =>
+                        {:kindergarten =>
+                          "./data/Kindergartners in full-day program.csv"}
+                        })
+    @ha = HeadcountAnalyst.new(@dr)
+  end
 
-    assert_instance_of HeadcountAnalyst,ha
+  def test_it_exisit
+    assert_instance_of HeadcountAnalyst,@ha
+  end
+
+  def test_enrollment_analysis_basics
+    skip
+    dr = DistrictRepository.new
+    dr.load_data({:enrollment => {:kindergarten => "./data/Kindergartners in full-day program.csv"}})
+    ha = HeadcountAnalyst.new(dr)
+    assert_in_delta 1.126, ha.kindergarten_participation_rate_variation("GUNNISON WATERSHED RE1J", :against => "TELLURIDE R-1"), 0.005
+    assert_in_delta 0.447, ha.kindergarten_participation_rate_variation('ACADEMY 20', :against => 'YUMA SCHOOL DISTRICT 1'), 0.005
   end
 end
