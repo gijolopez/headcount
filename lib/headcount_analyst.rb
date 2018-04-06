274938e8bd4da58e5158e859dc8e8795b3fde136
gijolopez <glopez22@me.com>
2018-04-06 17:43:36 -0600

    addd average kinder participation per district

--- lib/headcount_analyst.rb
+++ lib/headcount_analyst.rb
@@ -1,12 +1,19 @@
 require_relative '../lib/district_repository'
-
+require 'pry'
 class HeadcountAnalyst
   attr_reader :dr
   def initialize(dr)
     @dr = dr
   end
 
-  def kindergarten_participation_rate_variation(data,d)
+  def average_kindergarten_participation_per_district(name)
+    district = @dr.find_by_name(name)
+    total = district.enrollment.kindergarten_participation_by_year
+                               .reduce(0) {|sum, percent| sum + percent[1]}
+    average = total / district.enrollment.kindergarten_participation_by_year.length
+  end
+
+  def kindergarten_participation_rate_variation(name,data)
 
   end
 end
