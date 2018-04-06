a29d0e0f5f6d86dda2623dd001047c3da84ba9b8
gijolopez <glopez22@me.com>
2018-04-06 15:23:47 -0600

    adding kindergarten_part_rate_variation method

--- lib/headcount_analyst.rb
+++ lib/headcount_analyst.rb
@@ -1,2 +1,12 @@
+require_relative '../lib/district_repository'
+
 class HeadcountAnalyst
+  attr_reader :dr
+  def initialize(dr)
+    @dr = dr
+  end
+
+  def kindergarten_participation_rate_variation(data,d)
+
+  end
 end
