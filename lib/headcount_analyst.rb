56eebb83a0134fe70403dd2f4836406b018058e3
gijolopez <glopez22@me.com>
2018-04-06 19:23:21 -0600

    finished Iteration 1

--- lib/headcount_analyst.rb
+++ lib/headcount_analyst.rb
@@ -17,14 +17,27 @@ class HeadcountAnalyst
                       .reduce(0) {|sum, percent| sum + percent[1]}
 
     average = total / district.enrollment.kindergarten_participation_by_year.length
-    
+
     convert_to_three_decimals(average)
   end
 
-  def kindergarten_participation_rate_variation(name,data)
+  def kindergarten_participation_rate_variation(name, data)
     district_1 = average_kindergarten_participation_per_district(name)
     district_2 = average_kindergarten_participation_per_district(data[:against])
     variation_rate = district_1 / district_2
     convert_to_three_decimals(variation_rate)
+  end
+
+  def kindergarten_participation_rate_variation_trend(name, data)
+    district_1 = @dr.find_by_name(name)
+    district_1_data = district_1.enrollment.kindergarten_participation_by_year
+
+    district_2 = @dr.find_by_name(data[:against])
+    district_2_data = district_2.enrollment.kindergarten_participation_by_year
+
+    variation = district_1_data.merge(district_2_data) do |key, oldval, newval|
+    variation_trend = oldval / newval
+    convert_to_three_decimals(variation_trend)
+   end
   end
 end
