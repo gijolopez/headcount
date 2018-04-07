3c44d78e3416a14b4b3133fb22b09f75a6cc603f
gijolopez <glopez22@me.com>
2018-04-06 18:37:50 -0600

    added variation rate method

--- lib/headcount_analyst.rb
+++ lib/headcount_analyst.rb
@@ -1,7 +1,10 @@
 require_relative '../lib/district_repository'
 require 'pry'
+
 class HeadcountAnalyst
+
   attr_reader :dr
+
   def initialize(dr)
     @dr = dr
   end
@@ -11,9 +14,16 @@ class HeadcountAnalyst
     total = district.enrollment.kindergarten_participation_by_year
                                .reduce(0) {|sum, percent| sum + percent[1]}
     average = total / district.enrollment.kindergarten_participation_by_year.length
+    (average.to_f*1000).floor/1000.0
   end
 
   def kindergarten_participation_rate_variation(name,data)
+    dist1 = average_kindergarten_participation_per_district(name)
 
+    dist2 = average_kindergarten_participation_per_district(data[:against])
+
+    variation_rate = dist1 / dist2
+
+    (variation_rate.to_f*1000).floor/1000.0
   end
 end
