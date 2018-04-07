b33186c547da8e79880eed379e7e4ac37a5fbe6c
gijolopez <glopez22@me.com>
2018-04-06 19:24:08 -0600

    finished Iteration 1

--- test/headcount_analyst_test.rb
+++ test/headcount_analyst_test.rb
@@ -29,7 +29,17 @@ class HeadcountAnalystTest < Minitest::Test
   # end
 
   def test_kindergarten_participation_rate_variation
+    assert_equal 0.766, @ha.kindergarten_participation_rate_variation('ACADEMY 20', :against => 'COLORADO')
     assert_equal 1.126, @ha.kindergarten_participation_rate_variation("GUNNISON WATERSHED RE1J", :against => "TELLURIDE R-1")
     assert_equal 0.446, @ha.kindergarten_participation_rate_variation('ACADEMY 20', :against => 'YUMA SCHOOL DISTRICT 1')
+  end
+
+  def test_kindergarten_participation_rate_variation_trend
+    actual = @ha.kindergarten_participation_rate_variation_trend('ACADEMY 20',:against => 'COLORADO')
+    expected = {2004 => 1.258, 2005 => 0.96, 2006 => 1.05, 2007 => 0.992,
+                2008 => 0.717, 2009 => 0.652, 2010 => 0.681, 2011 => 0.727,
+                2012 => 0.687, 2013 => 0.693, 2014 => 0.661 }
+
+    assert_equal expected, actual
   end
 end
