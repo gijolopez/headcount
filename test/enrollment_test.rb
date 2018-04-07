835e0145032f97e551cf7671d0758bca8e463f67
gijolopez <glopez22@me.com>
2018-04-07 12:56:29 -0600

    adding graduation_rate_by_year test method setup

--- test/enrollment_test.rb
+++ test/enrollment_test.rb
@@ -8,7 +8,7 @@ class EnrollmentTest < Minitest::Test
                           :kindergarten_participation =>
                                     {2010 => 0.3915, 2011 => 0.35356, 2012 => 0.2677}})
 
-end
+  end
 
   def test_it_exists
     assert_instance_of Enrollment, @e
@@ -35,5 +35,25 @@ end
 
   def test_kindergarten_participation_in_year
     assert_equal 0.391, @e.kindergarten_participation_in_year(2010)
+  end
+
+  def test_graduation_rate_by_year
+
+  end
+
+  def test_enrollment_repository_with_high_school_data
+    er = EnrollmentRepository.new
+    er.load_data({ :enrollment => {
+                        :kindergarten => "./data/Kindergartners in full-day program.csv",
+                              :high_school_graduation => "./data/High school graduation rates.csv"
+                   }})
+    e = er.find_by_name("MONTROSE COUNTY RE-1J")
+
+
+    expected = {2010=>0.738, 2011=>0.751, 2012=>0.777, 2013=>0.713, 2014=>0.757}
+    expected.each do |k,v|
+    assert_in_delta v, e.graduation_rate_by_year[k], 0.005
+    end
+    assert_in_delta 0.738, e.graduation_rate_in_year(2010), 0.005
   end
 end
