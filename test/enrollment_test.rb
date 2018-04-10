d672323088539dd68bf92a3f85e2dad5d6135ce0
gijolopez <glopez22@me.com>
2018-04-10 16:17:17 -0600

    added a couple of test to check graduation_rate_in_year & graduation_rate_by_year, I also restructured the def setup for this test class

--- test/enrollment_test.rb
+++ test/enrollment_test.rb
@@ -1,5 +1,7 @@
 require_relative 'test_helper'
 require_relative '../lib/enrollment'
+require_relative '../lib/enrollment_repository'
+
 
 class EnrollmentTest < Minitest::Test
 
@@ -7,9 +9,13 @@ class EnrollmentTest < Minitest::Test
     @e = Enrollment.new({
       :name => "ACADEMY 20",
           :kindergarten_participation =>
-              {2010 => 0.3915, 2011 => 0.35356, 2012 => 0.2677},
-                    :high_school_graduation =>
-                          {2010=>0.738, 2011=>0.751, 2012=>0.777, 2013=>0.713, 2014=>0.757}})
+              {2010 => 0.3915, 2011 => 0.35356, 2012 => 0.2677}})
+
+    @er = EnrollmentRepository.new
+    @high_school_data = ({ :enrollment => {
+        			                :kindergarten => './data/Kindergartners in full-day program.csv',
+        				                  :high_school_graduation => './data/High school graduation rates.csv'}
+                                  })
   end
 
   def test_it_exists
@@ -40,32 +46,23 @@ class EnrollmentTest < Minitest::Test
     assert_equal 0.391, @e.kindergarten_participation_in_year(2010)
   end
 
-  # def test_it_has_attributes_2
-  #   @e = er.find_by_name("MONTROSE COUNTY RE-1J")
-  #   assert_equal "ACADEMY 20", @e.name
-  #   assert_instance_of Hash, @e.high_school_graduation_rates
-  # end
-
   def test_graduation_rate_by_year
-    expected = {2010=>0.738, 2011=>0.751, 2012=>0.777, 2013=>0.713, 2014=>0.757}
-    actual = @e.graduation_rate_by_year
+    @er.load_data(@high_school_data)
+
+    enrollment = @er.find_by_name("ACADEMY 20")
+
+    expected = {2010 => 0.895, 2011 => 0.895, 2012 => 0.889, 2013 => 0.913, 2014 => 0.898}
+    actual = enrollment.graduation_rate_by_year
 
     assert_equal expected, actual
   end
 
-  def test_enrollment_repository_with_high_school_data
-    # er = EnrollmentRepository.new
-    # er.load_data({ :enrollment => {
-    #                     :kindergarten => "./data/Kindergartners in full-day program.csv",
-    #                           :high_school_graduation => "./data/High school graduation rates.csv"
-    #                }})
-    # e = er.find_by_name("MONTROSE COUNTY RE-1J")
+  def test_graduation_rate_in_year
+    @er.load_data(@high_school_data)
 
-    # expected = {2010=>0.738, 2011=>0.751, 2012=>0.777, 2013=>0.713, 2014=>0.757}
-    # actual = @e.graduation_rate_by_year
-    #
-    # assert_equal expected, actual
-    end
-  #   assert_in_delta 0.738, e.graduation_rate_in_year(2010), 0.005
-  # end
+    enrollment = @er.find_by_name("ACADEMY 20")
+
+    assert_equal 0.898, enrollment.graduation_rate_in_year(2014)
+  end
+
 end
