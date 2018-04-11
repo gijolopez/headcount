4dbdcdcc30c333beb5bb9c9188d013be5ee94812
gijolopez <glopez22@me.com>
2018-04-10 18:47:33 -0600

    added a few more test hs_rate_variation & hs_grad_per_district

--- test/headcount_analyst_test.rb
+++ test/headcount_analyst_test.rb
@@ -41,12 +41,17 @@ class HeadcountAnalystTest < Minitest::Test
     assert_equal expected, actual
   end
 
-  def test_high_school_versus_kindergarten_analysis
-    dr = DistrictRepository.new
-    dr.load_data({:enrollment => {:kindergarten => "./data/Kindergartners in full-day program.csv",
-                                  :high_school_graduation => "./data/High school graduation rates.csv"}})
-    ha = HeadcountAnalyst.new(dr)
+  def test_average_high_school_graduation_per_district
+    average = @ha.average_high_school_graduation_per_district("ACADEMY 20")
+    assert_equal 0.898, average
+  end
 
+  def test_high_school_graduation_rate_variation
+    assert_equal 1.195, @ha.high_school_graduation_rate_variation('ACADEMY 20', :against => 'COLORADO')
+  end
+
+  def test_kindergarten_participation_against_high_school_graduation
+    assert_equal 0.641, @ha.kindergarten_participation_against_high_school_graduation('ACADEMY 20')
     assert_equal 0.548, @ha.kindergarten_participation_against_high_school_graduation('MONTROSE COUNTY RE-1J')
     assert_equal 0.800, @ha.kindergarten_participation_against_high_school_graduation('STEAMBOAT SPRINGS RE-2')
   end
