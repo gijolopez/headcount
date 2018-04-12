4676803878aae64b5afacc07217649da864b2952
gijolopez <glopez22@me.com>
2018-04-12 10:17:21 -0600

    added statewide_test to initialize and to attr_accessor

--- lib/district.rb
+++ lib/district.rb
@@ -1,9 +1,11 @@
 class District
-  attr_reader :name
-  attr_accessor :enrollment
+  attr_reader   :name
+  attr_accessor :enrollment,
+                :statewide_test
 
   def initialize(data)
-    @name       = data[:name]
-    @enrollment = nil
+    @name           = data[:name]
+    @enrollment     = nil
+    @statewide_test = nil
   end
 end
