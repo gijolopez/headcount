36fccb7cf646f128b9daa55877675d1bf2cfac15
gijolopez <glopez22@me.com>
2018-04-12 23:28:36 -0600

    added economic profile relationship

--- lib/district.rb
+++ lib/district.rb
@@ -1,11 +1,13 @@
 class District
   attr_reader   :name
   attr_accessor :enrollment,
-                :statewide_test
+                :statewide_test,
+                :economic_profile
 
   def initialize(data)
-    @name           = data[:name]
-    @enrollment     = nil
-    @statewide_test = nil
+    @name             = data[:name]
+    @enrollment       = nil
+    @statewide_test   = nil
+    @economic_profile = nil
   end
 end
