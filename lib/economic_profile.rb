b08f021b17aac0185bda17b2999ee1ae5e6ea694
gijolopez <glopez22@me.com>
2018-04-12 23:43:37 -0600

    still need to refactor, added mulitple methods for ep and thier test.

---
+++ lib/economic_profile.rb
@@ -0,0 +1,70 @@
+require 'csv'
+require_relative 'raise_error'
+
+class EconomicProfile
+  attr_reader :name, :median_household_income, :children_in_poverty,
+              :free_or_reduced_price_lunch, :title_i
+
+  def initialize(data)
+    @name = data[:name]
+    @median_household_income = data[:median_household_income] || Hash.new
+
+    @children_in_poverty = data[:children_in_poverty] || Hash.new
+
+    @free_or_reduced_price_lunch =
+      data[:free_or_reduced_price_lunch] || Hash.new
+
+    @title_i = data[:title_i] || Hash.new
+  end
+
+  def median_household_income_in_year(year)
+    keys_for_use = @median_household_income.keys.map do |key|
+      key if (key[0]..key[1]).to_a.include?(year)
+    end
+
+    raise UnknownDataError if keys_for_use.compact.count == 0
+    incomes = keys_for_use.compact.map do |key|
+      @median_household_income[key]
+    end
+    (incomes.inject(:+) / incomes.count)
+  end
+
+  def median_household_income_average
+    (@median_household_income.values.inject(:+)/
+    @median_household_income.values.count)
+  end
+
+  def children_in_poverty_in_year(year)
+    if @children_in_poverty[year].nil?
+      raise UnknownDataError
+
+    else
+      @children_in_poverty[year]
+    end
+  end
+
+  def free_or_reduced_price_lunch_percentage_in_year(year)
+    if @free_or_reduced_price_lunch[year].nil?
+      raise UnknownDataError
+
+    else
+      @free_or_reduced_price_lunch[year][:percentage]
+    end
+  end
+
+  def free_or_reduced_price_lunch_number_in_year(year)
+    if @free_or_reduced_price_lunch[year].nil?
+      raise UnknownDataError
+    else
+      @free_or_reduced_price_lunch[year][:total]
+    end
+  end
+
+  def title_i_in_year(year)
+    if @title_i[year].nil?
+      raise UnknownDataError
+    else
+      @title_i[year]
+    end
+  end
+end
---
+++ test/economic_profile_test.rb
@@ -0,0 +1,72 @@
+require_relative 'test_helper'
+require_relative '../lib/economic_profile_repository'
+require_relative '../lib/economic_profile'
+
+
+class EconomicProfileTest < Minitest::Test
+
+  def setup
+  	@epr = EconomicProfileRepository.new
+  	@loaded ||= @epr.load_data({
+        		  :economic_profile => {
+      		    :median_household_income => "./data/Median household income.csv",
+      		    :children_in_poverty => "./data/School-aged children in poverty.csv",
+      		    :free_or_reduced_price_lunch => "./data/Students qualifying for free or reduced price lunch.csv",
+      		    :title_i => "./data/Title I students.csv"
+    		}
+  		})
+    @data = {:median_household_income => {[2005, 2009] => 50000, [2008, 2014] => 60000},
+            :children_in_poverty => {2012 => 0.1845},
+            :free_or_reduced_price_lunch => {2014 => {:percentage => 0.023, :total => 100}},
+            :title_i => {2015 => 0.543},
+            :name => "ACADEMY 20"
+       }
+  	@profile = @epr.find_by_name('ACADEMY 20')
+  end
+
+  def test_it_can_initialize
+    profile = EconomicProfile.new(@data)
+    assert_instance_of EconomicProfile, profile
+  end
+
+  def test_it_can_find_median_household_income_by_year
+    assert_instance_of EconomicProfile, @profile
+    assert_equal 87635, @profile.median_household_income_in_year(2009)
+    assert_equal 85060, @profile.median_household_income_in_year(2005)
+    assert_raises UnknownDataError do
+      @profile.median_household_income_in_year(2000)
+    end
+  end
+
+  def test_median_household_income_average_method
+    assert_equal 87635, @profile.median_household_income_average
+  end
+
+  def test_children_in_poverty_in_year_method
+    assert_equal 0.064, @profile.children_in_poverty_in_year(2012)
+    assert_raises UnknownDataError do
+      @profile.children_in_poverty_in_year(1993)
+    end
+  end
+
+  def test_free_or_reduced_price_lunch_percentage_in_year
+    assert_equal 0.103, @profile.free_or_reduced_price_lunch_percentage_in_year(2009)
+    assert_raises UnknownDataError do
+      @profile.free_or_reduced_price_lunch_percentage_in_year(1993)
+    end
+  end
+
+  def test_free_or_reduced_price_lunch_number_in_year
+    assert_equal 2058, @profile.free_or_reduced_price_lunch_number_in_year(2008)
+    assert_raises UnknownDataError do
+      @profile.free_or_reduced_price_lunch_number_in_year(1993)
+    end
+  end
+
+  def test_title_i_in_year
+    assert_equal 0.01, @profile.title_i_in_year(2012)
+    assert_raises UnknownDataError do
+      @profile.title_i_in_year(1993)
+    end
+  end
+end
